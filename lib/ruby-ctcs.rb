3a4d3612973b114ec0ae33226e1984bc39605672
Aaron Herting <aaron@herting.cc>
2014-04-18 13:45:20 -0400

    added regex to parse status p2

--- lib/ruby-ctcs.rb
+++ lib/ruby-ctcs.rb
@@ -3,6 +3,13 @@ require_relative 'ruby-ctcs/server.rb'
 require_relative 'ruby-ctcs/client.rb'
 
 module CTCS
+
+	STATUS_P2 = [ /^(?<seeders>\d+):(?<t_seeders>\d+)\/(?<leechers>\d+):(?<t_leechers>\d+)\/(?<connecting>\d+)$/,
+								/^(?<n_have>\d+)\/(?<n_total>\d+)\/(?<n_avail>\d+)$/,
+								/^(?<dl_rate>\d+),(?<ul_rate>\d+)$/,
+								/^(?<dl_total>\d+),(?<ul_total>\d+)$/,
+								/^(?<dl_limit>\d+),(?<ul_limit>\d+)$/,
+								/^(?<cache_used>\d+)$/ ]
 	
 end
 
