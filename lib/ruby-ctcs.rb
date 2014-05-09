d9ac987a13bd982844e0c6f114c4be10534f995a
Aaron Herting <aaron@herting.cc>
2014-05-09 11:54:28 -0400

    added ctdetail regex

--- lib/ruby-ctcs.rb
+++ lib/ruby-ctcs.rb
@@ -5,7 +5,11 @@ require_relative 'ruby-ctcs/client.rb'
 module CTCS
 
 	STATUS_P2 = /^(?<seeders>\d+):(?<t_seeders>\d+)\/(?<leechers>\d+):(?<t_leechers>\d+)\/(?<connecting>\d+) (?<n_have>\d+)\/(?<n_total>\d+)\/(?<n_avail>\d+) (?<dl_rate>\d+),(?<ul_rate>\d+) (?<dl_total>\d+),(?<ul_total>\d+) (?<dl_limit>\d+),(?<ul_limit>\d+) (?<cache_used>\d+)$/ 
+
 	BW = /^(?<dl_rate>\d+),(?<ul_rate>\d+) (?<dl_limit>\d+),(?<ul_limit>\d+)$/
+
+	CTDETAIL = /^(?<torrent_size>\d+) (?<piece_size>\d+) (?<current_time>\d+) (?<seed_time>\d+)$/
+
 
 	PROTOCOL_VERSION = "0003"
 	
